a0e38d56dbb46fc529fc843561a55f093e10a698
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2018-01-07 23:12:42 +0800

    Adds helpers for date and url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def fix_url(str)
+    if str.starts_with?('http://') || str.starts_with?('https://')
+      str
+    else
+      "https://#{str}"
+    end
+  end
+
+  def display_datetime(dt)
+    dt.strftime("%m/%d/%Y %l:%M%P %Z")
+  end
 end
