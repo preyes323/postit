6ed6a15fe5e9f95802472da3b81a6192e2c07c00
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2018-01-12 00:46:27 +0800

    Completes session; WIP: use authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,17 @@
 class SessionsController < ApplicationController
-  def new
-
-  end
+  def new; end
 
   def create
+    user = User.find_by username: params[:username]
 
+    if user && user.authenticate(params[:password])
+      flash[:notice] = "Welcome, you've logged in."
+      session[:user] = user.id
+      redirect_to root_path
+    else
+      flash[:error] = 'There is something wrong with your username or password.'
+      render 'new'
+    end
   end
 
   def destroy
